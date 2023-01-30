afcb26e390aab91002d9dabfbf3151d095b382c7
Matt Bougie <matt@agrograph.com>
2023-01-30 09:28:45 -0600

    added raster layer to map

--- react-map-gl-example/pages/map.tsx
+++ react-map-gl-example/pages/map.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Map from "@/components/Map";
-
 import ControlPanel from "@/components/ControlPanel";
+import RasterLayer from "@/components/Map/RasterLayer";
 import { GetStaticProps } from "next";
 import { Layer, Source } from "react-map-gl";
 
@@ -42,6 +42,23 @@ export default function Home(props: any) {
                   ],
                 },
                 "fill-opacity": 0.8,
+              }}
+            />
+          </Source>
+          <Source
+            type="image"
+            url="https://docs.mapbox.com/mapbox-gl-js/assets/radar.gif"
+            coordinates={[
+              [-80.425, 46.437],
+              [-71.516, 46.437],
+              [-71.516, 37.936],
+              [-80.425, 37.936],
+            ]}
+          >
+            <Layer
+              type="raster"
+              paint={{
+                "raster-fade-duration": 0,
               }}
             />
           </Source>
